0d5ea50fb3e7efe960bde1869a2fe7d026b050a5
Akhil Raja <p.akhilraja@gmail.com>
2020-10-31 22:07:51 +0530

    Gun added

--- Assets/Scripts/AntiAliasing.cs
+++ Assets/Scripts/AntiAliasing.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class antialiansingcorrection : MonoBehaviour
+public class AntiAliasing : MonoBehaviour
 {
     void Start()
     {
--- Assets/Scripts/FollowARCamera.cs
+++ Assets/Scripts/FollowARCamera.cs
@@ -7,38 +7,35 @@ public class FollowARCamera : MonoBehaviour
 {
     public Transform aRCoreCamera;
 
-    public Text tulisan;
+    public Text debugText;
 
     Vector3 smoothedPosition;
     private Vector3 startPosition;
     private Vector3 endPosition;
-    private float speed = 7.5f; //2.5 works also with some slow down
+    private float speed = 7.5f; 
     private float maxSpeed;
 
-    // 0.0159 is the distance the camera is away from the head. This leads to position changes when turning the had and we wanna avoid that. The current setting need still more refinement. 
+    //Magic number that works well
     private Vector3 headRotationCorrection = new Vector3(0, 0, 0.0159f);
 
 
     public float mirrorAngle = 0;
     private Quaternion deltaPosition;
 
-    // the update method is twice as fast as the ARCore Camera. To avoid jittering we use Lerp to smooth out the movement at the start and the end of the movement.
     void Update()
     {
-
-        //correct the delta vector by given angle
         if (mirrorAngle != 0f)
         {
             deltaPosition = Quaternion.AngleAxis(mirrorAngle, Vector3.up) * deltaPosition;
         }
 
         startPosition = this.transform.position;
-        endPosition = aRCoreCamera.transform.position - headRotationCorrection; // TODO not sure if headRotationCorrection even helps.
+        endPosition = aRCoreCamera.transform.position - headRotationCorrection; 
         maxSpeed = speed * Time.deltaTime;
-        // Using Lerp this way is a trick. TODO: There are probably better solutions which should also be tested and might reduce lag further.
+        //Lerping to smooth out the movement
         smoothedPosition = Vector3.Lerp(startPosition, endPosition, maxSpeed);
 
         this.transform.position = smoothedPosition;
-        tulisan.text = smoothedPosition.ToString();
+        debugText.text = smoothedPosition.ToString();
     }
 }
